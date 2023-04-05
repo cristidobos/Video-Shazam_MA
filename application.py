ea0769a1467222be5b72925c846d4a7b2c502e96
Alex Grigore <42294736+AlexxGrigore@users.noreply.github.com>
2023-04-05 15:26:18 +0200

    add wav

--- application.py
+++ application.py
@@ -1,7 +1,9 @@
 import video_query
-import CropVideo
-from VideoReader import Video
+# import CropVideo
+# from VideoReader import Video
 import FileReader
+
+
 def main():
     print("Please provide the path of the video query:")
     print("example: dataset/Asteroid_Discovery")
@@ -10,11 +12,11 @@ def main():
     database = "database/videos.sqlite"
 
     # Store query as a video object
-    video = FileReader.VideoReader(query_path)
-    audio = FileReader.AudioReader(query_path)
+    video = FileReader.VideoReader(query_path + ".mp4")
+    audio = FileReader.AudioReader(query_path + ".wav")
 
     # Crop the video only to contain the video contents
-    #cropped_video = CropVideo.crop_video(video)
+    # cropped_video = CropVideo.crop_video(video)
 
     print("Parsed query")
     # Get descriptor for query video
@@ -24,7 +26,6 @@ def main():
     name, score, frame = video_query.find_best_match(descriptor, database, video.fps)
 
     print("Best match was found for video {} with a score of {} at frame {}".format(name, score, frame))
-
 
 
 if __name__ == '__main__':
